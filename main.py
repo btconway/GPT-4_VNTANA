34ef9318d7f5a0c2beefbf00c77c8099857e67ee
btconway <ben@vntana.com>
2023-08-10 06:31:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ st.set_page_config(page_title="VNTANA Sales", page_icon="Profile_Avatar.jpg")
 st.sidebar.image("Profile_Avatar.jpg")
 st.info("`I am an AI that can help you generate sales and marketing content. For example, I can write email sequences, generate marketing copy, and more."
     "To get the best results, include VNTANA in your request and I can access VNTANA's vector database. You can also say somethign like 'use your tools'`")
+st.cache_resource.clear()
+
 
 from typing import Any, List, Optional, Sequence, Tuple, Union, Type
 import json
